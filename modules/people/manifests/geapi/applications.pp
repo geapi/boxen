5e239aa9c53519ec34e6ea6aa59ad93bb935efdd
Georg Apitz <pair+georg@pivotallabs.com>
2015-09-21 14:55:08 -0600

    Add more apps

--- modules/people/manifests/geapi/applications.pp
+++ modules/people/manifests/geapi/applications.pp
@@ -1,8 +1,27 @@
 class people::geapi::applications { 
   include textmate::textmate2::release 
+  include iterm2::stable
+  include flux
+  include onepassword
+  include chrome
+  include divvy
+  include googledrive
+  include evernote
+  include dropbox
+  include utorrent
+  include vlc
+  include calibre
+  include atom
+  include firefox
+  include omnigraffle::pro
+  include screenhero
   
   package { 
     'alfred': provider => 'brewcask' 
+  }
+  
+  class { 'vlc': 
+    version => '2.1.5'
   }
 }
 
