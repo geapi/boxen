a024c0f51a794a580c7345ad1eef4053db08981b
Georg Apitz <pair+georg@pivotallabs.com>
2015-09-23 21:04:21 -0600

    remove extra line and wrong text

--- modules/people/manifests/geapi/osx/menubarclock.pp
+++ modules/people/manifests/geapi/osx/menubarclock.pp
@@ -3,6 +3,7 @@ class people::geapi::osx::menubarclock (
   $date_format = "EEE MMM d H.mm.ss"
 ) {
   include people::geapi::osx::systemuiserver
+  
   boxen::osx_defaults { 'Set time format for menubar clock':
     user      => $::boxen_user,
     key       => 'DateFormat',
@@ -11,6 +12,4 @@ class people::geapi::osx::menubarclock (
     type      => 'string',
     notify => Exec['killall SystemUIServer'];
   }
-  
-  killall -KILL SystemUIServer
 }
